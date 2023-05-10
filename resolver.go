212099f6b5f86bcb110ad34faef97b0b128f4784
Seokseong Jeon <6747391+somidad@users.noreply.github.com>
2023-05-11 00:46:11 +0900

    Create resolver.go

---
+++ resolver.go
@@ -0,0 +1,34 @@
+package wikilink
+
+import "path/filepath"
+var QuartzResolver Resolver = quartzResolver{}
+
+type Resolver interface {
+	ResolveWikilink(*Node) (destination []byte, err error)
+}
+
+var _html = []byte(".html")
+
+type quartzResolver struct{}
+
+func (quartzResolver) ResolveWikilink(n *Node) ([]byte, error) {
+	dest := make([]byte, len(n.Target)+len(_html)+len(_hash)+len(n.Fragment))
+	var i int
+	if len(n.Target) > 0 {
+		i += copy(dest, n.Target)
+		if filepath.Ext(string(n.Target)) == "" {
+			i += copy(dest[i:], _html)
+		}
+	}
+	if len(n.Fragment) > 0 {
+		i += copy(dest[i:], _hash)
+    for f := 0; f < len(n.Fragment); f++ {
+      if n.Fragment[f] == '.' {
+        continue
+      }
+      dest[i] = n.Fragment[f]
+      i++
+    }
+	}
+	return dest[:i], nil
+}
